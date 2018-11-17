de9e4b97a84224f475fa3f6f863c3758ecab9ff1
saranyasowndarajan <45116155+saranyasowndarajan@users.noreply.github.com>
2018-11-17 12:03:28 +0530

    Create Positive.java

---
+++ java-programs/Positive.java
@@ -0,0 +1,16 @@
+import java.io.*;
+class PositiveNegative
+{
+public static void main(String args[])
+{
+int a=3;
+if(a>3)
+{
+System.out.println("positive");
+}
+else 
+{
+System.out.println("Negative");
+}
+}
+}
